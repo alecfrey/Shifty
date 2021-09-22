e231f695740b4efdbc9dda55dfef6c472dfaecca
alecfrey <79166968+alecfrey@users.noreply.github.com>
2021-09-22 14:03:20 -0500

    Update LoadingScreen.java

--- core/src/com/mygdx/shifty/LoadingScreen.java
+++ core/src/com/mygdx/shifty/LoadingScreen.java
@@ -2,28 +2,6 @@ package com.mygdx.shifty;
 
 import com.badlogic.gdx.Screen;
 
-<<<<<<< HEAD
-/**
- * LoadingScreen class used between title screen and game screen.
- * 
- * @author Alec Frey, Tyler Atkinson, Nathan Cook
- *
- */
-public class LoadingScreen implements Screen {
-	private ShiftyGame parent;
-
-	public LoadingScreen(ShiftyGame screen) {
-		parent = screen;
-	}
-
-	@Override
-	public void show() {
-
-	}
-
-	/**
-	 * Changes screen to main menu after application starts
-=======
 public class LoadingScreen implements Screen{
 	private ShiftyGame parent;
 	
@@ -37,40 +15,11 @@ public class LoadingScreen implements Screen{
 	}
 	/**
 	 * changes screen to main menu after application starts
->>>>>>> c38133729d121c25fe36687fd48987b5320cd1f1
 	 */
 	@Override
 	public void render(float delta) {
 		parent.changeScreen(ShiftyGame.MENU);
 	}
-<<<<<<< HEAD
-
-	@Override
-	public void resize(int width, int height) {
-
-	}
-
-	@Override
-	public void pause() {
-
-	}
-
-	@Override
-	public void resume() {
-
-	}
-
-	@Override
-	public void hide() {
-
-	}
-
-	@Override
-	public void dispose() {
-
-	}
-}
-=======
 	
 	@Override
 	public void resize(int width, int height) {
@@ -97,4 +46,3 @@ public class LoadingScreen implements Screen{
 		
 	}
 }
->>>>>>> c38133729d121c25fe36687fd48987b5320cd1f1
